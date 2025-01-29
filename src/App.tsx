ea45b08c433107d567eca79f2ff78d0020f7f321
caerroff <56608891+caerroff@users.noreply.github.com>
2025-01-29 15:02:36 +0200

    feat(front): change front and add URI to spotify

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { loginUrl } from './utils/spotifyLogin'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [loggedIn, setLoggedIn] = useState(false)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Spotify Exporter</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <a href={loginUrl} id='signInButton' target="_blank"><button className="spotify-color">Log In with Spotify</button></a>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {loggedIn ? 
+          <></>
+          : 
+          <span className="italic">You are currently not logged in...</span> 
+          }
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
